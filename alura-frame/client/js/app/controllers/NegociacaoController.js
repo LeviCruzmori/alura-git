ea2142a45f53cc19e6f2e759338471cbcbc369a3
levimori <dsciencemori@gmail.com>
2020-02-07 02:15:30 -0300

    CONTROLLER.JS restrturado conforme aula5

--- alura-frame/client/js/app/controllers/NegociacaoController.js
+++ alura-frame/client/js/app/controllers/NegociacaoController.js
@@ -7,27 +7,32 @@ class NegociacaoController {
         this._inputData = $('#data')
         this._inputQuantidades = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = ListaNegociacoes();//função chamada em index.
 
     }
 
     adiciona(event){
         event.preventDefault();
-
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-          
-        let negociacao = new Negociacao(
-        DateHelper.textoParaData(this._inputData.value),  
-        this._inputQuantidades.value,
-        this._inputValor.value
-      );
-        console.log(negociacao);
-       console.log(DateHelper.dataParaTexto(negociacao.data));
+        this._listaNegociacoes.adiciona(this._criaNegociacao());//função adiciona e limpa formulario
+        this._limpaFormulario();   
     }
-}
+    
+        _criaNegociacao(){
+            return new Negociacao(
+                DateHelper.textoParaData(this._inputData.value),
+                this._inputQuantidades.value,
+                this._inputValor.value
+            );
+        }  
+        _limpaFormulario(){
+            this._inputData.value = '';
+            this._inputQuantidade.value = 1;
+            this._inputValor.value = 0.0;
+            this._inputData.focus(); 
+        }
+
+    }
+
 //  console.log(typerof(this._inutDate.value));// informa qual é o tipo de sintaxe que esta sendo exibida, neste caso o inputDate esta devolvendo uma string, gerando conflito com  a sintaxe do getTime.
       ///   console.log(this._inputData.value)
 
